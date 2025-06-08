621f0192bae81e2241146a2d96305c1de839d6df
smas7832 <mukrramalishah5050@gmail.com>
2025-06-08 20:46:17 +0530

    Project Main fn

--- Projects/Ticket System/main.c
+++ Projects/Ticket System/main.c
@@ -6,17 +6,38 @@
                     -Ability To reschedule
                 2-Ewallet system
                     -Ability to deposit, withdraw
+        Flowchart: https://excalidraw.com/#json=J1-kzw-6jBQf_Iz5Z_ilw,RUl6XdgD927zZdv_u9VqVQ
  */
 #include <stdio.h>
 
-int tktsstm(){
-
-
-
+int ticket(){
+    int choice;
+    do{
+        printf("\n-------------\n");
+        printf("1.Book Ticket\n2.Booked Tickets\n3.Cancel Booking");
+        printf("Select Choice:");
+        scanf("%d", &choice);
+        switch(choice){
+            case 1:bookTkt();
+            case 2:bookedTkt();
+            case 3:cancelTkt();
+            default: printf("Invalid Choice."); 
+        }
+    }while(choice != 4);
 }
 
 int main(){
-
-
-
+    int choice;
+    do{
+        printf("\n-------------\n");
+        printf("1.Ticket Section\n2.Wallet\n3.Exit");
+        printf("Select Choice:");
+        scanf("%d", &choice);
+        switch(choice){
+            case 1: ticket();
+            case 2: wallet();
+            case 3: printf("\nExiting...\n");
+            default: printf("Invalid choice");
+        }
+    }while (choice != 3);
 }
