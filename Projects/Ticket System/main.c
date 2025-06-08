f6cba3aaa711f880ce4765425d8fcba967054761
smas7832 <mukrramalishah5050@gmail.com>
2025-06-08 21:37:38 +0530

    Initial logic for ticket

--- Projects/Ticket System/main.c
+++ Projects/Ticket System/main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /*
         Ticket Management system with:
                 1-Ticket Booking system:
@@ -8,36 +10,127 @@
                     -Ability to deposit, withdraw
         Flowchart: https://excalidraw.com/#json=J1-kzw-6jBQf_Iz5Z_ilw,RUl6XdgD927zZdv_u9VqVQ
  */
-#include <stdio.h>
 
-int ticket(){
-    int choice;
-    do{
-        printf("\n-------------\n");
-        printf("1.Book Ticket\n2.Booked Tickets\n3.Cancel Booking");
-        printf("Select Choice:");
-        scanf("%d", &choice);
-        switch(choice){
-            case 1:bookTkt();
-            case 2:bookedTkt();
-            case 3:cancelTkt();
-            default: printf("Invalid Choice."); 
-        }
-    }while(choice != 4);
-}
+struct train
+{
+    int no;
+    char name[20];
+    int capacity;
+    int seatavl;
+};
 
-int main(){
+/*------Ticket Booking system------*/
+int bookTkt(struct train *tr); // To Be done
+int bookedTkt();               // TBD
+int cancelTkt();               // TBD
+int ticket();                  // Half Done
+
+/*------Ewallet system------*/
+int wallet();
+
+/*------Main------*/
+int main()
+{
     int choice;
-    do{
+    do
+    {
         printf("\n-------------\n");
         printf("1.Ticket Section\n2.Wallet\n3.Exit");
-        printf("Select Choice:");
+        printf("\nSelect Choice:");
         scanf("%d", &choice);
-        switch(choice){
-            case 1: ticket();
-            case 2: wallet();
-            case 3: printf("\nExiting...\n");
-            default: printf("Invalid choice");
+        switch (choice)
+        {
+        case 1:
+            ticket();
+            break;
+        case 2:
+            wallet();
+            break;
+        case 3:
+            printf("\nExiting...\n");
+            break;
+        default:
+            printf("Invalid choice");
+            break;
         }
-    }while (choice != 3);
+    } while (choice != 3);
+    return 0;
+}
+
+/*------Ticket Booking system------*/
+int ticket()
+{
+    struct train tr[10];
+    int choice;
+    do
+    {
+        printf("\n-------------\n");
+        printf("1.Book Ticket\n2.Booked Tickets\n3.Cancel Booking");
+        printf("\nSelect Choice:");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            bookTkt(&tr);
+            break;
+        case 2:
+            bookedTkt();
+            break;
+
+        case 3:
+            cancelTkt();
+            break;
+
+        default:
+            printf("Invalid Choice.");
+            break;
+        }
+    } while (choice != 4);
+}
+
+int bookTkt(struct train *tr)
+{
+    printf("Trains Available are:\n");
+    for (int i = 0; i<10; i++){
+        printf("Train No: %d", tr->no);
+        printf("Train Name: %s", tr->name);
+        printf("Train Available Seats: %d", tr->seatavl);
+    }
+
+}
+int bookedTkt()
+{
+    printf("Enter the Train no. to book ticket\n");
+}
+int cancelTkt()
+{
+    printf("Enter the Train no. to book ticket\n");
+}
+
+/*------Ewallet system------*/
+int wallet()
+{
+    int choice;
+    do
+    {
+        printf("\n-------------\n");
+        printf("1.Book Ticket\n2.Booked Tickets\n3.Cancel Booking");
+        printf("\nSelect Choice:");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            break;
+
+        case 2:
+            bookedTkt();
+            break;
+        case 3:
+            cancelTkt();
+            break;
+        default:
+            printf("Invalid Choice.");
+            break;
+        }
+    } while (choice != 4);
 }
