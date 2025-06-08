8892f477fcd6ccd1100f5767ba65106e6acd5b79
smas7832 <mukrramalishah5050@gmail.com>
2025-06-08 19:46:55 +0530

    New Project

---
+++ Projects/Ticket System/main.c
@@ -0,0 +1,9 @@
+/*
+        Ticket Management system with:
+                1-Ticket Booking system:
+                    -Able To Booking
+                    -Ability Cancel ticket
+                    -Ability To reschedule
+                2-Ewallet system
+                    -Ability to deposit, withdraw
+ */
