1b7b82773981e6c8c99ed9c6069ba75f6ee26887
smas7832 <mukrramalishah5050@gmail.com>
2025-06-12 21:21:21 +0530

    minor aditions

--- Python/pax.py
+++ Python/pax.py
@@ -3,7 +3,7 @@ import json # Import the json library to handle potential JSON response
 
 # Replace 'YOUR_API_KEY' with your actual API key
 api_key = None
-url = "https://api.paxsenix.biz.id/ai-image/flux"
+url = "https://api.paxsenix.biz.id/lyrics/plain"
 
 data = {
   "model": "pax-uncensor",
@@ -14,13 +14,16 @@ data = {
     }
   ]
 }
-
+params = {
+    "title": "Ek Raat",
+    "artist": "Vilen" 
+}
 headers = {
   "Authorization": f"Bearer {api_key}",
   "Content-Type": "application/json"
 }
 
-response = rq.post(url, params=data)
+response = rq.post(url, params=params)
 print(response.url)
 response.raise_for_status
 print(response.status_code)
