dbff2b25e27df2854a5b97d49139e4c45c234e4f
smas7832 <mukrramalishah5050@gmail.com>
2025-07-02 18:50:42 +0530

    Test

---
+++ Projects/Ticket System/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//For test codes
+
+void json_write(char string[], char filename[]){
+    FILE *fp = fopen(filename, "a   +");
+    if(fp == NULL){
+        perror("Error occured:");
+    }
+    fprintf(fp,string, stdin);
+
+}
+
+
+int main(){
+    char master[] = "data.json";
+
+    for(int i; i<100; i++)
+    {json_write("\nLovly day", master);}
+}
