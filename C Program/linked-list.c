df2f085dc391b4ede4c6d254459743ffde1f5b34
smas7832 <mukrramalishah5050@gmail.com>
2025-08-09 21:17:22 +0530

    ll

--- C Program/linked-list.c
+++ C Program/linked-list.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-// Singly Linked list 
+// Singly Linked list
 void* HEAD;
 
 typedef struct{
@@ -10,21 +10,33 @@ typedef struct{
 
 //fn for creating node
 
-void *createNode(int value){
+void *createNode(int value, nodes *node){
 	nodes *newNode;
+	node->next = &newNode;
 	newNode->data = value;
 	newNode->next = NULL;
 	return &newNode;
 }
 
 int main(){
-	nodes *node= (nodes*)calloc(5, sizeof(nodes));;
-	//node[5] 
-	HEAD = &node[1];
-	node[1]->next = createNode(1);
-	for(int i = 2; i<5;i++){
-		node[i-1]->next = createNode(i);
+	nodes *node[5];
+
+	for (int i = 0; i<5; i++)
+	{
+		node[i] = malloc(sizeof(nodes));
+		printf("\n node %d : %p", i, node[i]);
 	}
+
+	HEAD = &node[0];
 	printf("\n HEAD : %p", HEAD);
-	//printf("\n node1 : %p", node);
+	printf("\n NODE1 : %p", node[0]);
+	// node[1]->next = createNode(1);
+	for(int i = 1; i<5;i++){
+		node[i] = createNode(i, node[i-1]);
+		printf("\n node1 : %p", node[i]);
+		printf("\n HEAD : %p", node);
+}
+	for(int i = 0; i<5; i++){
+		free(node[i]);
+	}
 }
