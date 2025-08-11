a47bd3b4cf07078d2f982b30702144a41217b95c
smas7832 <mukrramalishah5050@gmail.com>
2025-08-11 17:38:22 +0530

    ll

--- C Program/linked-list.c
+++ C Program/linked-list.c
@@ -9,13 +9,18 @@ typedef struct{
 }nodes;
 
 //fn for creating node
-
 void *createNode(int value, nodes *node){
 	nodes *newNode;
 	node->next = &newNode;
 	newNode->data = value;
 	newNode->next = NULL;
 	return &newNode;
+}
+
+//fn to delete node
+void *deleteNode(nodes * node, nodes *pvsNode){
+	 pvsNode->next = node->next;
+	 free(node);
 }
 
 int main(){
@@ -26,6 +31,8 @@ int main(){
 		node[i] = malloc(sizeof(nodes));
 		printf("\n node %d : %p", i, node[i]);
 	}
+	deleteNode(node[4], node[3]);
+	printf("\n deleted node : %p", node[4]);
 
 	HEAD = &node[0];
 	printf("\n HEAD : %p", HEAD);
@@ -36,7 +43,7 @@ int main(){
 		printf("\n node1 : %p", node[i]);
 		printf("\n HEAD : %p", node);
 }
-	for(int i = 0; i<5; i++){
-		free(node[i]);
-	}
+// for(int i = 0; i<5; i++){
+// 	free(node[i]);
+// 	}
 }
