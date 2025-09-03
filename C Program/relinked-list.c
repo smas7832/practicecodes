1a48ba2a6307d853ee37caa3cefecca62b609027
SMAS <mukrramalishah5050@gmail.com>
2025-09-03 12:02:38 +0530

    successfully implemented insertnode and prinlist

--- C Program/relinked-list.c
+++ C Program/relinked-list.c
@@ -1,19 +1,46 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 struct node{
   int data;
   struct node *next;
 };
 
-void newNode(int data, struct node *head){
-  struct node *temp = HEAD;
+void insertNode(int data, struct node **HEAD){
+  struct node *newNode =(struct node *)malloc(sizeof(struct node));
+  newNode-> data = data;
+  newNode-> next = NULL;
+  if (*HEAD == NULL){
+    *HEAD = newNode;
+    return;
+  }
+
+  struct node *temp = *HEAD;
   while (temp->next != NULL){
     temp = temp->next;
   }
-  struct node *node =(struct node *)malloc(sizeof(struct node))
+  temp->next = newNode;
 }
 
- int main(){
+void printList(struct node *HEAD){
+  int i = 0;
+  struct node *temp = HEAD;
+  printf("Node\t\tData\t\tAdress\n");
 
+  while(temp->next !=NULL){
+    printf("%d\t\t%d\t\t%p\n",i,temp->data,temp);
+    temp = temp->next;
+    i +=1;
+  }
+  printf("NULL\n");
+}
+
+ int main(int argc, char *argv[]){
+   struct node *HEAD = NULL;
+   int n = atoi(argv[1]);
+   for(int i = 0; i<=n; i++){
+     insertNode(i+i*i, &HEAD);
+   }
+  printList(HEAD);
 
 }
