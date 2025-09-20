8836f4555bee01b07a42639e5a50fccc52c52da5
SMAS <mukrramalishah5050@gmail.com>
2025-09-20 19:50:15 +0530

    formating

--- C Program/AgeC.c
+++ C Program/AgeC.c
@@ -1,25 +1,26 @@
-#include<stdio.h>
-//To Calculate Age with current year being 2024
-int calculateAge(int userInput){
-    int currentYear = 2024; 
-    int age = currentYear - userInput;
-    int years = age / 100;
-    int decades = age % 100;
-    if (years < 18) {
-        return printf("You are too young to become an adult\n");
-    } else if (years > 18) {
-        return printf("It's common for people to reach their late 20s as an adult.\n");
-    } else if (years >= 60) {
-        return printf("Congratulations! You are now an Old Person.\n");
-    }
+#include <stdio.h>
+// To Calculate Age with current year being 2024
+int calculateAge(int userInput) {
+  int currentYear = 2024;
+  int age = currentYear - userInput;
+  int years = age / 100;
+  int decades = age % 100;
+  if (years < 18) {
+    return printf("You are too young to become an adult\n");
+  } else if (years > 18) {
+    return printf(
+        "It's common for people to reach their late 20s as an adult.\n");
+  } else if (years >= 60) {
+    return printf("Congratulations! You are now an Old Person.\n");
+  }
+  return 0;
 }
 
-int main(){
-    int userInput;
-    printf("Please enter your age: ");
-    scanf("%d", &userInput);
-    
-    printf("You're %d years old. ", calculateAge(userInput));
-    
-    return 0;
+int main() {
+  int userInput;
+  printf("Please enter your age: ");
+  scanf("%d", &userInput);
+
+  return 0;
 }
+
--- C Program/relinked-list.c
+++ C Program/relinked-list.c
@@ -1,73 +1,70 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-struct node{
+struct node {
   int data;
   struct node *next;
 };
 
-void randomSeed(){
-  srand(time(NULL));
-}
+void randomSeed() { srand(time(NULL)); }
 
 int randomInt(int min, int max) {
-	int rd_num = rand() % (max - min + 1) + min;
-	return rd_num;
+  int rd_num = rand() % (max - min + 1) + min;
+  return rd_num;
 }
-void insertNode(int data, struct node **HEAD){
-  struct node *newNode =(struct node *)malloc(sizeof(struct node));
-  newNode-> data = data;
-  newNode-> next = NULL;
-  if (*HEAD == NULL){
+void insertNode(int data, struct node **HEAD) {
+  struct node *newNode = (struct node *)malloc(sizeof(struct node));
+  newNode->data = data;
+  newNode->next = NULL;
+  if (*HEAD == NULL) {
     *HEAD = newNode;
     return;
   }
 
   struct node *temp = *HEAD;
-  while (temp->next != NULL){
+  while (temp->next != NULL) {
     temp = temp->next;
   }
   temp->next = newNode;
 }
 
-void freeList(struct node *HEAD){
+void freeList(struct node *HEAD) {
   struct node *current = HEAD;
   struct node *nextNode;
-  while(current != NULL){
+  while (current != NULL) {
     nextNode = current->next;
     free(current);
     current = nextNode;
   }
 }
 
-void deleteNode(struct node **HEAD, struct node *delete){
-    if (*HEAD == NULL && delete == NULL){
-      printf("Error: Empty list or invalid node");
-      return;
-    }
+void deleteNode(struct node **HEAD, struct node *delete) {
+  if (*HEAD == NULL && delete == NULL) {
+    printf("Error: Empty list or invalid node");
+    return;
+  }
 
-    if (*HEAD == delete){
-      *HEAD=delete->next;
-      free(delete);
-      printf("Head Node deleted\n");
-      return;
-    }
-
-
-    struct node *pvsNode = *HEAD;
-    while(pvsNode->next != NULL && pvsNode->next != delete){
-      pvsNode = pvsNode->next;
-    }
-    pvsNode->next = delete->next;
+  if (*HEAD == delete) {
+    *HEAD = delete->next;
     free(delete);
+    printf("Head Node deleted\n");
+    return;
+  }
+
+  struct node *pvsNode = *HEAD;
+  while (pvsNode->next != NULL && pvsNode->next != delete) {
+    pvsNode = pvsNode->next;
+  }
+  pvsNode->next = delete->next;
+  free(delete);
 }
 
-int searchNode(struct node *HEAD, int target){
-  struct node *current=HEAD;
+int searchNode(struct node *HEAD, int target) {
+  struct node *current = HEAD;
   int count = 0;
-  while(current !=NULL){
-    if(current->data == target){
+  while (current != NULL) {
+    if (current->data == target) {
       return count;
     }
     current = current->next;
@@ -76,8 +73,8 @@ int searchNode(struct node *HEAD, int target){
   return -1;
 }
 
-void printList(struct node *HEAD){
-  if(HEAD==NULL){
+void printList(struct node *HEAD) {
+  if (HEAD == NULL) {
     printf("\nEmpty list");
     return;
   }
@@ -85,27 +82,27 @@ void printList(struct node *HEAD){
   struct node *temp = HEAD;
   printf("Node\tData\tAdress\t\t\tnextNode\n");
 
-  while(temp !=NULL){
-    printf("%d\t%d\t%p\t\t%p\n",i,temp->data,temp,temp->next);
+  while (temp != NULL) {
+    printf("%d\t%d\t%p\t\t%p\n", i, temp->data, temp, temp->next);
     temp = temp->next;
-    i +=1;
+    i += 1;
   }
   printf("\n\t-----------------END-----------------\n");
 }
 
- int main(int argc, char *argv[]){
-   struct node *HEAD = NULL;
-   int n = atoi(argv[1]);
-   int target = atoi(argv[2]);
-   randomSeed();
-   for(int i = 0; i<n; i++){
-     insertNode(randomInt(i,i*10), &HEAD);
-   }
+int main(int argc, char *argv[]) {
+  struct node *HEAD = NULL;
+  int n = atoi(argv[1]);
+  int target = atoi(argv[2]);
+  randomSeed();
+  for (int i = 0; i < n; i++) {
+    insertNode(randomInt(i, i * 10), &HEAD);
+  }
   printList(HEAD);
   int result = searchNode(HEAD, target);
-  if (result == -1){
+  if (result == -1) {
     printf("\nCouldn't find the node");
-  }else{
+  } else {
     printf("\nnode is at index: %d ", result);
   }
   freeList(HEAD);
